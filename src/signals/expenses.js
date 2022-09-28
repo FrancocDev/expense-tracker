5620e151e2e4d02a36e6b84824aaa0d1c6a37637
Franco Carrara <frankko212@gmail.com>
2022-09-28 19:43:16 -0300

    Added signals

---
+++ src/signals/expenses.js
@@ -0,0 +1,4 @@
+import { signal } from "@preact/signals-react";
+
+const expenses = signal([]);
+export default expenses;
---
+++ src/signals/modal.js
@@ -0,0 +1,4 @@
+import { signal } from "@preact/signals-react";
+
+const modal = signal(false);
+export default modal;
