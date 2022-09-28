18a31cfd9793240a5975b787d446acc0faebdeba
Franco Carrara <frankko212@gmail.com>
2022-09-28 19:45:13 -0300

    Header depends on signals

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import NewExpense from './NewExpense'
+import BudgetOverview from './BudgetOverview'
+import budget from '../signals/budget'
 
 function Header(){
     return(
         <header className="pt-8 bg-blue-500  text-center h-80">
             <span className="pt-10 tracking-wide text-white font-semibold uppercase text-3xl">Expense tracker</span>
-            <NewExpense />
+            {budget.value ? <BudgetOverview/> : <NewExpense />}
         </header>
     )
 }
