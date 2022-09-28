797791502275789733f6c0ff426e20d0da85aefa
Franco Carrara <frankko212@gmail.com>
2022-09-28 19:44:26 -0300

    Add expense and modal

---
+++ src/components/AddExpense.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import PlusIcon from '../assets/plus-icon.svg'
+import modal from '../signals/modal'
+
+function AddExpense() {
+    function openModal(){
+        modal.value = true
+    }
+
+  return (
+    <div onClick={openModal} className='fixed right-4 bottom-4  bg-blue-500 rounded-full p-3 text-center  stroke-white fill-white'>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="#FFF" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+        </svg>
+    </div>
+  )
+}
+
+export default AddExpense
+
+
