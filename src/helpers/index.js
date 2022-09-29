dc174f46d8fa358d74ca99eb9a59d6aaea3bdca1
Franco Carrara <frankko212@gmail.com>
2022-09-29 19:55:43 -0300

    add Date and capitalize

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,16 @@
 export function formatToCurrency(value){
     return value.toLocaleString('en-US', {style: "currency", currency: "USD"})
 }
+
+export function formatDate(date){
+    const options = {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+    }
+    return date.toLocaleDateString('en-US', options)
+}
+
+export function capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
