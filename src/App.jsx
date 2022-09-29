23b0c1a0eebf984d394a1af28bd73269e9f46670
Franco Carrara <frankko212@gmail.com>
2022-09-29 19:55:28 -0300

    Fix conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AddExpense from './components/AddExpense'
 import modal from './signals/modal'
 import Modal from './components/Modal'
 import ExpensesList from './components/ExpensesList'
+import expenses from './signals/expenses'
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
       <Header />
       {Boolean(budget.value) && <AddExpense/>}
       {Boolean(modal.value) && <Modal/>}
-      {Boolean(budget.value) && <ExpensesList/>}
+      {Boolean(expenses.value.length) && <ExpensesList/>}
     </>
   )
 }
